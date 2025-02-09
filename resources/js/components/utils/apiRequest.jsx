d2281c1ea718006a55287afbdd5e6e856271bb5f
shakibzaman <zaman.shakib@gmail.com>
2025-02-09 18:24:11 +0600

    Fix

--- resources/js/components/utils/apiRequest.jsx
+++ resources/js/components/utils/apiRequest.jsx
@@ -4,7 +4,7 @@ export default async function apiRequest(
     body = null,
     isFormData = false
 ) {
-    const API_BASE_URL = "http://localhost:8000/api"; // API Base URL
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api"; // Get from env with fallback
     const token = localStorage.getItem("token");
 
     const config = {
