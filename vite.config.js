1a9f60c7697d5eede9be9a243b95b9be49028423
shakibzaman <zaman.shakib@gmail.com>
2025-02-09 17:35:21 +0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,25 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'path';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => {
-    const config = {
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/index.jsx'],
-                refresh: true,
-            }),
-            react(),
-        ],
-        build: {
-            manifest: true,
-            outDir: 'public/build',
-            rollupOptions: {
-                input: {
-                    app: resolve(__dirname, 'resources/js/index.jsx'),
-                    css: resolve(__dirname, 'resources/css/app.css')
-                }
-            }
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx','resources/css/app.css'], // Ensure this points to the correct entry file
+            refresh: true,
+        }),
+        react(), // Handles JSX
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js', // Optional: Set up alias for cleaner imports
         },
-        resolve: {
-            alias: {
-                '@': '/resources/js',
-            },
-            extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        },
-        css: {
-            postcss: {
-                plugins: [tailwindcss()]
-            }
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    css: {
+        postcss: {
+          plugins: [tailwindcss()]
         }
-    };
-
-    // Development specific config
-    if (command !== 'build') {
-        config.server = {
-            historyApiFallback: true,
-            host: '0.0.0.0',
-            port: 5173,
-            strictPort: true,
-            hmr: {
-                host: 'localhost',
-            },
-            watch: {
-                usePolling: true,
-                interval: 300,
-            },
-        };
-    }
-
-    return config;
+      }
 });
